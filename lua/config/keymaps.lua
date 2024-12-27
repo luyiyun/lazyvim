26f936eeb15dce1602d6aede81535df130a24757
zhiwei rong <18845728185@163.com>
2024-12-27 13:42:53 +0800

    add keymap for no hightlight, disable session manager

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,9 @@
--- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Keymaps are automatically loaded on the VeryLazy event
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>fs", function()
   require("persistence").select()
 end, { desc = "Select Session" })
+
+vim.keymap.set("n", "-", ":noh<cr>", { desc = "remove the search highlight" })
