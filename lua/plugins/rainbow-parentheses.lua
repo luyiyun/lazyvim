233d4adb847b6dc42f4c868ac2cc46108c6c6dd3
zhiwei rong <18845728185@163.com>
2024-12-29 14:23:03 +0800

    use true rainbow-delimiters setup function

--- lua/plugins/rainbow-parentheses.lua
+++ lua/plugins/rainbow-parentheses.lua
@@ -2,5 +2,9 @@ return {
   "HiPhish/rainbow-delimiters.nvim",
   event = "BufRead",
   depencencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {},
+  config = function()
+    -- NOTE: the configure function of rainbow-delimiters is not
+    -- same as other plugins, it doesn't have a setup function
+    require("rainbow-delimiters.setup").setup({})
+  end,
 }
