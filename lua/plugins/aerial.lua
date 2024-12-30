ad197a61711915366987d7e78654af2460236962
zhiwei rong <18845728185@163.com>
2024-12-30 12:35:53 +0800

    config blink to disable cmdline completion, add aerial to show the symbol outline in the right column and config the width of the column

---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,7 @@
+return {
+  "stevearc/aerial.nvim",
+  opts = function(_, opts)
+    opts.layout["max_width"] = { 40, 0.3 }
+    opts.layout["min_width"] = 20
+  end,
+}
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -31,6 +31,7 @@ return {
             -- },
           },
         },
+        cmdline = {},
       }
     end,
   },
